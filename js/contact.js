84f7d792eab58b2e6d2e6288acd419496670c105
kingqk@163.com <kingqk@gmail.com>
2021-09-26 23:49:11 +0900

    メール送信処理を追加

--- js/contact.js
+++ js/contact.js
@@ -1,46 +1,91 @@
 // Contact Form
+// AcroReach.co.jp 金全凱　2021-09-26
 function validateForm() {
-  var name = document.forms["myForm"]["name"].value;
-  var email = document.forms["myForm"]["email"].value;
+  //お問合せ内容
   var subject = document.forms["myForm"]["subject"].value;
-  var comments = document.forms["myForm"]["comments"].value;
+  //会社名
+  var companyNm = document.forms["myForm"]["companyNm"].value;
+  //会社名フリガナ
+  var companyKanaNm = document.forms["myForm"]["companyKanaNm"].value;
+  //住　所
+  var address = document.forms["myForm"]["address"].value;
+  //電話番号(ハイフンなし) 
+  var telno = document.forms["myForm"]["telno"].value;
+  //メールアドレス
+  var email = document.forms["myForm"]["email"].value;
+  //担当名
+  var contactor = document.forms["myForm"]["contactor"].value;
+  //ホームページもしくはネットショップリング
+  var hpLink = document.forms["myForm"]["hpLink"].value;
+  //ご要望
+  var message = document.forms["myForm"]["message"].value;
   document.getElementById("error-msg").style.opacity = 0;
   document.getElementById('error-msg').innerHTML = "";
-  if (name == "" || name == null) {
-    document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Name*</div>";
-    fadeIn();
+  if (subject == "" || subject == null) {
+    showErrorMsg("問い合わせ内容を選択してください。");
+    return false;
+  }
+  if (companyNm == "" || companyNm == null) {
+    showErrorMsg("会社名を入力してください。");
+    return false;
+  }
+  if (companyKanaNm == "" || companyKanaNm == null) {
+    showErrorMsg("フリガナを入力してください。");
+    return false;
+  }
+  if (address == "" || address == null) {
+    showErrorMsg("住所を入力してください。");
+    return false;
+  }
+  if (telno == "" || telno == null) {
+    showErrorMsg("電話番号を入力してください。");
     return false;
   }
   if (email == "" || email == null) {
-    document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Email*</div>";
-    fadeIn();
+    showErrorMsg("メールアドレスを入力してください。");
     return false;
   }
-  if (subject == "" || subject == null) {
-    document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Subject*</div>";
-    fadeIn();
+  if (contactor == "" || contactor == null) {
+    showErrorMsg("担当名を入力してください。");
     return false;
   }
-  if (comments == "" || comments == null) {
-    document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*Please enter a Comments*</div>";
-    fadeIn();
+  if (hpLink == "" || hpLink == null) {
+    showErrorMsg("ホームページもしくはネットショップリングを入力してください。");
+    return false;
+  }
+  if (message == "" || message == null) {
+    showErrorMsg("ご要望を入力してください。");
     return false;
   }
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("simple-msg").innerHTML = this.responseText;
-      document.forms["myForm"]["name"].value = "";
-      document.forms["myForm"]["email"].value = "";
-      document.forms["myForm"]["subject"].value = "";
-      document.forms["myForm"]["comments"].value = "";
+      // document.forms["myForm"]["subject"].value = "";
+      // document.forms["myForm"]["companyNm"].value = "";
+      // document.forms["myForm"]["companyKanaNm"].value = "";
+      // document.forms["myForm"]["address"].value = "";
+      // document.forms["myForm"]["telno"].value = "";
+      // document.forms["myForm"]["email"].value = "";
+      // document.forms["myForm"]["contactor"].value = "";
+      // document.forms["myForm"]["hpLink"].value = "";
+      // document.forms["myForm"]["message"].message = "";
     }
   };
   xhttp.open("POST", "php/contact.php", true);
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhttp.send("name=" + name + "&email=" + email + "&subject=" + subject + "&comments=" + comments);
+  xhttp.send("subject=" + subject + "&companyNm=" + companyNm + 
+  "&companyKanaNm=" + companyKanaNm + "&address=" + address + 
+  "&telno=" + telno + "&email=" + email + 
+  "&contactor=" + contactor + "&hpLink=" + hpLink + "&message=" + message);
   return false;
 }
+
+function showErrorMsg(msg){
+  document.getElementById('error-msg').innerHTML = "<div class='alert alert-warning error_message'>*" + msg +  "*</div>";
+  fadeIn();
+}
+
 function fadeIn() {
   var fade = document.getElementById("error-msg");
   var opacity = 0;
--- php/contact.php
+++ php/contact.php
@@ -2,63 +2,112 @@
 
 if(!$_POST) exit;
 
-// Email address verification, do not edit.
+
 function isEmail($email) {
 	return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i",$email));
 }
 
 if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
 
-$name     = $_POST['name'];
+$subject     = $_POST['subject'];
+$companyNm     = $_POST['companyNm'];
+$companyKanaNm     = $_POST['companyKanaNm'];
+$address     = $_POST['address'];
+$telno     = $_POST['telno'];
 $email    = $_POST['email'];
-$comments = $_POST['comments'];
+$contactor    = $_POST['contactor'];
+$hpLink    = $_POST['hpLink'];
+$message = $_POST['message'];
 
-if(trim($name) == '') {
-	echo '<div class="error_message">You must enter your name.</div>';
+if(trim($subject) == '') {
+	echo '<div class="error_message">問い合わせ内容を選択してください。</div>';
 	exit();
-} else if(trim($email) == '') {
-	echo '<div class="error_message">Please enter a valid email address.</div>';
+} 
+if(trim($companyNm) == '') {
+	echo '<div class="error_message">会社名を入力してください。</div>';
 	exit();
-} else if(!isEmail($email)) {
-	echo '<div class="error_message">You have entered an invalid e-mail address. Please try again.</div>';
+}
+if(trim($companyKanaNm) == '') {
+	echo '<div class="error_message">フリガナを入力してください。</div>';
+	exit();
+}
+if(trim($address) == '') {
+	echo '<div class="住所を入力してください。</div>';
+	exit();
+}
+if(trim($telno) == '') {
+	echo '<div class="error_message">電話番号を入力してください。</div>';
+	exit();
+}
+if(trim($email) == '') {
+	echo '<div class="error_message">メールアドレスを入力してください。</div>';
+	exit();
+}if(!isEmail($email)) {
+	echo '<div class="error_message">メールアドレスのフォーマットが間違い、再度入力してください。</div>';
+	exit();
+}
+if(trim($contactor) == '') {
+	echo '<div class="error_message">担当名を入力してください。</div>';
+	exit();
+}
+if(trim($hpLink) == '') {
+	echo '<div class="error_message">ホームページもしくはネットショップリングを入力してください。</div>';
+	exit();
+}
+if(trim($message) == '') {
+	echo '<div class="error_message">ご要望を入力してください。</div>';
 	exit();
 }
 
-if(trim($comments) == '') {
-	echo '<div class="error_message">Please enter your message.</div>';
-	exit();
-}
-
-if(get_magic_quotes_gpc()) {
-	$comments = stripslashes($comments);
-}
-
-
-// Configuration option.
-// Enter the email address that you want to emails to be sent to.
-// Example $address = "joe.doe@yourdomain.com";
-
-//$address = "example@example.net";
-$address = "example@example.net";
+//if(get_magic_quotes_gpc()) {
+	$companyNm = stripslashes($companyNm);
+	$companyKanaNm = stripslashes($companyKanaNm);
+	$address = stripslashes($address);
+	$contactor = stripslashes($contactor);
+	$hpLink = stripslashes($hpLink);
+	$message = stripslashes($message);
+//}
 
 
-// Configuration option.
-// i.e. The standard subject will appear as, "You've been contacted by John Doe."
+//実際の送信先を設定する
+$send_to_mail_address = "king@test.com";
 
-// Example, $e_subject = '$name . ' has contacted you via Your Website.';
+//(1)問い合わせ内容を送信
+// メールタイトル
+$e_subject = '【ホームページ問い合わせ】 ' . $companyNm . 'からのご要望';
 
-$e_subject = 'You have been contacted by ' . $name . '.';
+
+// メール本文
+//$e_body = "You have been contacted by $companyNm. Their additional message is as follows." . PHP_EOL . PHP_EOL;
+//$e_content = "\"$message\"" . PHP_EOL . PHP_EOL;
+//$e_reply = "You can contact $companyNm via email, $email";
+
+$data = date("Y-m-d H:i:s");
+$hpLink=wordwrap($hpLink,10,"\r\n");
+$message=wordwrap($message,10,"\r\n");
+
+$msg = "
+＜＜ホームページからの問い合わせ＞＞
 
 
-// Configuration option.
-// You can change this if you feel that you need to.
-// Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
+＝＝＝＝＝お問い合わせ内容＝＝＝＝＝
+①お問い合わせ内容：$subject
+②会社名　　　　　：$companyNm 
+③フリガナ　　　　：$companyKanaNm
+④住所　　　　　　：$address
+⑤電話番号　　　　：$telno
+⑥メールアドレス　：$email
+⑦メールアドレス　：$contactor
+⑧ホームページもしくはネットショッピング：
+　　　　　　　　　　$hpLink
+⑨ご要望　　　　　：
+　　　　　　　　　　$message
+⑩お問い合わせ日時：$data
+＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+";
 
-$e_body = "You have been contacted by $name. Their additional message is as follows." . PHP_EOL . PHP_EOL;
-$e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
-$e_reply = "You can contact $name via email, $email";
 
-$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
+//$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
 
 $headers = "From: $email" . PHP_EOL;
 $headers .= "Reply-To: $email" . PHP_EOL;
@@ -66,14 +115,14 @@ $headers .= "MIME-Version: 1.0" . PHP_EOL;
 $headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
 $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 
-if(mail($address, $e_subject, $msg, $headers)) {
+if(mail($send_to_mail_address, $e_subject, $msg, $headers)) {
 
 	// Email has sent successfully, echo a success page.
 
 	echo "<fieldset>";
 	echo "<div id='success_page'>";
-	echo "<h5>Email Sent Successfully.</h5>";
-	echo "<p>Thank you <strong>$name</strong>, your message has been submitted to us.</p>";
+	echo "<h5>メール送信が成功しました。</h5>";
+	echo "<p><strong>$companyNm さま</strong>、ありがとうございました。 </p>";
 	echo "</div>";
 	echo "</fieldset>";
 
@@ -82,3 +131,53 @@ if(mail($address, $e_subject, $msg, $headers)) {
 	echo 'ERROR!';
 
 }
+
+
+//(2)自動返信処理
+// メールタイトル
+$e_subject = '【株式会社ジャトラコンサルティング】メッセージ受信完了のお知らせ';
+
+$data = date("Y-m-d H:i:s");
+$hpLink=wordwrap($hpLink,10,"\r\n");
+$message=wordwrap($message,10,"\r\n");
+
+$msg = "
+$companyNm さま
+
+この度は、株式会社ジャトラコンサルティングへお問い合わせいただきありがとうございます。
+下記の内容でメッセージを受け付けました。
+後日、担当者より改めてご連絡いたします。
+
+＜＜このメールはシステムによる自動送信です。このメールに直接返信することはできません。＞＞
+
+
+＝＝＝＝＝お問い合わせ内容＝＝＝＝＝
+①お問い合わせ内容：$subject
+②会社名　　　　　：$companyNm 
+③フリガナ　　　　：$companyKanaNm
+④住所　　　　　　：$address
+⑤電話番号　　　　：$telno
+⑥メールアドレス　：$email
+⑦メールアドレス　：$contactor
+⑧ホームページもしくはネットショッピング：
+　　　　　　　　　　$hpLink
+⑨ご要望　　　　　：
+　　　　　　　　　　$message
+⑩お問い合わせ日時：$data
+＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+
+
+-----------------------------------------------------
+株式会社ジャトラコンサルティング
+オンラインビジネスチーム
+-----------------------------------------------------
+";
+
+
+$headers = "From: $send_to_mail_address" . PHP_EOL;
+$headers .= "Reply-To: $send_to_mail_address" . PHP_EOL;
+$headers .= "MIME-Version: 1.0" . PHP_EOL;
+$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
+$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+
+mail($email, $e_subject, $msg, $headers);
