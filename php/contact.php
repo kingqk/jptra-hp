834f4922e18fc28dcca910cbdcd3f942b9f248be
kingqk@163.com <kingqk@gmail.com>
2021-10-11 01:03:05 +0900

    improve mail functions

--- php/contact.php
+++ php/contact.php
@@ -1,10 +1,18 @@
 <?php
 
-if(!$_POST) exit;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';
 
 
-function isEmail($email) {
-	return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i",$email));
+if (!$_POST) exit;
+
+function isEmail($email)
+{
+	return (preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i", $email));
 }
 
 if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
@@ -19,74 +27,96 @@ $contactor    = $_POST['contactor'];
 $hpLink    = $_POST['hpLink'];
 $message = $_POST['message'];
 
-if(trim($subject) == '') {
+if (trim($subject) == '') {
 	echo '<div class="error_message">問い合わせ内容を選択してください。</div>';
 	exit();
-} 
-if(trim($companyNm) == '') {
+}
+if (trim($companyNm) == '') {
 	echo '<div class="error_message">会社名を入力してください。</div>';
 	exit();
 }
-if(trim($companyKanaNm) == '') {
+if (trim($companyKanaNm) == '') {
 	echo '<div class="error_message">フリガナを入力してください。</div>';
 	exit();
 }
-if(trim($address) == '') {
+if (trim($address) == '') {
 	echo '<div class="住所を入力してください。</div>';
 	exit();
 }
-if(trim($telno) == '') {
+if (trim($telno) == '') {
 	echo '<div class="error_message">電話番号を入力してください。</div>';
 	exit();
 }
-if(trim($email) == '') {
+if (trim($email) == '') {
 	echo '<div class="error_message">メールアドレスを入力してください。</div>';
 	exit();
-}if(!isEmail($email)) {
+}
+if (!isEmail($email)) {
 	echo '<div class="error_message">メールアドレスのフォーマットが間違い、再度入力してください。</div>';
 	exit();
 }
-if(trim($contactor) == '') {
+if (trim($contactor) == '') {
 	echo '<div class="error_message">担当名を入力してください。</div>';
 	exit();
 }
-if(trim($hpLink) == '') {
+if (trim($hpLink) == '') {
 	echo '<div class="error_message">ホームページもしくはネットショップリングを入力してください。</div>';
 	exit();
 }
-if(trim($message) == '') {
+if (trim($message) == '') {
 	echo '<div class="error_message">ご要望を入力してください。</div>';
 	exit();
 }
 
 //if(get_magic_quotes_gpc()) {
-	$companyNm = stripslashes($companyNm);
-	$companyKanaNm = stripslashes($companyKanaNm);
-	$address = stripslashes($address);
-	$contactor = stripslashes($contactor);
-	$hpLink = stripslashes($hpLink);
-	$message = stripslashes($message);
+$companyNm = stripslashes($companyNm);
+$companyKanaNm = stripslashes($companyKanaNm);
+$address = stripslashes($address);
+$contactor = stripslashes($contactor);
+$hpLink = stripslashes($hpLink);
+$message = stripslashes($message);
 //}
 
 
-//実際の送信先を設定する
-$send_to_mail_address = "king@test.com";
+// 文字エンコードを指定
+mb_language('uni');
+mb_internal_encoding('UTF-8');
 
-//(1)問い合わせ内容を送信
-// メールタイトル
-$e_subject = '【ホームページ問い合わせ】 ' . $companyNm . 'からのご要望';
+// インスタンスを生成（true指定で例外を有効化）
+$mail = new PHPMailer(true);
+
+// 文字エンコードを指定
+$mail->CharSet = 'utf-8';
+
+try {
+	// デバッグ設定
+	// $mail->SMTPDebug = 2; // デバッグ出力を有効化（レベルを指定）
+	//  $mail->Debugoutput = function($str, $level) {echo "debug level $level; message: $str<br>";};
+
+	// SMTPサーバの設定
+	$mail->isSMTP();                          // SMTPの使用宣言
+	// $mail->Host       = 'smtp.gmail.com';   // SMTPサーバーを指定
+	$mail->Host       = 'localhost';   // SMTPサーバーを指定
+	// $mail->SMTPAuth   = true;                 // SMTP authenticationを有効化
+	// $mail->Username   = 'kikuchi.remi@jp-tra.co.jp';   // SMTPサーバーのユーザ名
+	// $mail->Password   = 'jp001-tra';           // SMTPサーバーのパスワード
+	// $mail->SMTPSecure = 'tls';  // 暗号化を有効（tls or ssl）無効の場合はfalse
+	// $mail->Port       = 587; // TCPポートを指定（tlsの場合は465や587）
+	$mail->Port       = 3025; // TCPポートを指定（tlsの場合は465や587）
+	//実際の送信先を設定する
+	$send_to_mail_address = "kikuchi.remi@jp-tra.co.jp";
+
+	//(1)問い合わせ内容を送信
+	// メールタイトル
+	$e_subject = '【ホームページ問い合わせ】 ' . $companyNm . 'からのご要望';
 
 
-// メール本文
-//$e_body = "You have been contacted by $companyNm. Their additional message is as follows." . PHP_EOL . PHP_EOL;
-//$e_content = "\"$message\"" . PHP_EOL . PHP_EOL;
-//$e_reply = "You can contact $companyNm via email, $email";
+	// メール本文
+	$data = date("Y-m-d H:i:s");
+	$hpLink = wordwrap($hpLink, 10, "\r\n");
+	$message = wordwrap($message, 10, "\r\n");
 
-$data = date("Y-m-d H:i:s");
-$hpLink=wordwrap($hpLink,10,"\r\n");
-$message=wordwrap($message,10,"\r\n");
-
-$msg = "
+	$msg = "
 ＜＜ホームページからの問い合わせ＞＞
 
 
@@ -107,41 +137,41 @@ $msg = "
 ";
 
 
-//$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
+	// 送受信先設定（第二引数は省略可）
+	$mail->setFrom($email, $companyNm); // 送信者
+	$mail->addAddress($send_to_mail_address);   // 宛先
+	$mail->addReplyTo($email, $companyNm); // 返信先
+	// $mail->addCC('cc@example.com', '受信者名'); // CC宛先
+	// $mail->Sender = 'kikuchi.remi@jp-tra.co.jp'; // Return-path
 
-$headers = "From: $email" . PHP_EOL;
-$headers .= "Reply-To: $email" . PHP_EOL;
-$headers .= "MIME-Version: 1.0" . PHP_EOL;
-$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
-$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+	// 送信内容設定
+	$mail->Subject = $e_subject;
+	$mail->Body    = $msg;
 
-if(mail($send_to_mail_address, $e_subject, $msg, $headers)) {
+	// 送信
+	if ($mail->send()) {
+		// Email has sent successfully, echo a success page.
 
-	// Email has sent successfully, echo a success page.
+		echo "<fieldset>";
+		echo "<div id='success_page'>";
+		echo "<h5>メール送信が成功しました。</h5>";
+		echo "<p><strong>$companyNm さま</strong>、ありがとうございました。 </p>";
+		echo "</div>";
+		echo "</fieldset>";
+	} else {
 
-	echo "<fieldset>";
-	echo "<div id='success_page'>";
-	echo "<h5>メール送信が成功しました。</h5>";
-	echo "<p><strong>$companyNm さま</strong>、ありがとうございました。 </p>";
-	echo "</div>";
-	echo "</fieldset>";
+		echo 'ERROR!';
+	}
 
-} else {
+	//(2)自動返信処理
+	// メールタイトル
+	$e_subject = '【株式会社ジャトラコンサルティング】メッセージ受信完了のお知らせ';
+	//メール本文
+	$data = date("Y-m-d H:i:s");
+	$hpLink = wordwrap($hpLink, 10, "\r\n");
+	$message = wordwrap($message, 10, "\r\n");
 
-	echo 'ERROR!';
-
-}
-
-
-//(2)自動返信処理
-// メールタイトル
-$e_subject = '【株式会社ジャトラコンサルティング】メッセージ受信完了のお知らせ';
-
-$data = date("Y-m-d H:i:s");
-$hpLink=wordwrap($hpLink,10,"\r\n");
-$message=wordwrap($message,10,"\r\n");
-
-$msg = "
+	$msg = "
 $companyNm さま
 
 この度は、株式会社ジャトラコンサルティングへお問い合わせいただきありがとうございます。
@@ -173,11 +203,29 @@ $companyNm さま
 -----------------------------------------------------
 ";
 
+	// $headers = "From: $send_to_mail_address" . PHP_EOL;
+	// $headers .= "Reply-To: $send_to_mail_address" . PHP_EOL;
+	// $headers .= "MIME-Version: 1.0" . PHP_EOL;
+	// $headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
+	// $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 
-$headers = "From: $send_to_mail_address" . PHP_EOL;
-$headers .= "Reply-To: $send_to_mail_address" . PHP_EOL;
-$headers .= "MIME-Version: 1.0" . PHP_EOL;
-$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
-$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+	// 送受信先設定（第二引数は省略可）
+	$mail->setFrom($send_to_mail_address, "株式会社ジャトラコンサルティング"); // 送信者
+	$mail->addAddress($email, $companyNm);   // 宛先
+	$mail->addReplyTo($send_to_mail_address, "株式会社ジャトラコンサルティング"); // 返信先
+	// $mail->addCC('cc@example.com', '受信者名'); // CC宛先
+	// $mail->Sender = 'kikuchi.remi@jp-tra.co.jp'; // Return-path
 
-mail($email, $e_subject, $msg, $headers);
+	// 送信内容設定
+	$mail->Subject = $e_subject;
+	$mail->Body    = $msg;
+
+	// 送信
+	$mail->send();
+} catch (Exception $e) {
+	// エラーの場合
+	echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+
+// mail($email, $e_subject, $msg, $headers);
